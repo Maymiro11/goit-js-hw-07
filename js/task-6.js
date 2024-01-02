4855b06fd7e1dce1df9e21ed93a25a688183950c
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-01-02 18:17:38 +0100

    task-5-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+function createBoxesHandler() {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert('Please enter a number between 1 and 100');
+  }
+
+  input.value = '';
+}
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxesHandler() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', createBoxesHandler);
+destroyButton.addEventListener('click', destroyBoxesHandler);
