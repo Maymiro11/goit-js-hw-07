42e07ffe47867c1160dee6874dc8825dca3a830a
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-01-04 18:04:40 +0100

    task-6- bag-fix

--- js/task-6.js
+++ js/task-6.js
@@ -4,37 +4,48 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const input = document.querySelector('input');
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]');
-const boxesContainer = document.querySelector('#boxes');
+const input = document.querySelector('#controls input');
+const createCard = document.querySelector('[data-create]');
+const destroyCard = document.querySelector('[data-destroy]');
+const boxForCard = document.querySelector('#boxes');
 
-function createBoxesHandler() {
-  const amount = Number(input.value);
-
-  if (amount >= 1 && amount <= 100) {
-    destroyBoxes();
-    createBoxes(amount);
-  } else {
-    alert('Please enter a number between 1 and 100');
+createCard.addEventListener('click', onButtonClick);
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    onButtonClick();
   }
+});
+destroyCard.addEventListener('click', destroyBoxes);
 
-  input.value = '';
+function onButtonClick() {
+  const amount = input.value;
+  if (amount > 0 && amount <= 100) {
+    return createBoxes(amount);
+  } else input.value = '';
+}
+function destroyBoxes() {
+  boxForCard.innerHTML = '';
 }
 
 function createBoxes(amount) {
-  for (let i = 1; i <= amount; i++) {
-    const box = document.createElement('div');
-    box.style.width = `${30 + (i - 1) * 10}px`;
-    box.style.height = `${30 + (i - 1) * 10}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+  boxForCard.innerHTML = '';
+  const cardArray = Array.from(
+    { length: amount },
+    (item) => (item = '<div class="card"></div>'),
+  ).join('\n\n');
+
+  boxForCard.insertAdjacentHTML('afterbegin', cardArray);
+
+  const cardStyle = Array.from(document.querySelectorAll('.card'));
+  let width = 30;
+  let height = 30;
+  for (let i = 0; i < cardStyle.length; i++) {
+    cardStyle[i].style.width = `${width}px`;
+    cardStyle[i].style.height = `${height}px`;
+    cardStyle[i].style.backgroundColor = getRandomHexColor();
+    width += 10;
+    height += 10;
   }
+  input.value = '';
 }
 
-function destroyBoxes() {
-  boxesContainer.innerHTML = '';
-}
-
-createButton.addEventListener('click', createBoxesHandler);
-destroyButton.addEventListener('click', destroyBoxesHandler);
